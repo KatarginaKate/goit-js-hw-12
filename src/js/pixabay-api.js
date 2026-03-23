1843bb9cc6fd1ce19bf98e597f298e22e60471b0
Katargina <katrin-kate@ukr.net>
2026-03-23 23:21:30 +0200

    pagination begin

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,15 +1,35 @@
 import axios from 'axios';
 
-export async function getImagesByQuery(query) {
-  const API_KEY = '55027621-850b52e35e959b3f4fd98aefc';
-  const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '55027621-850b52e35e959b3f4fd98aefc';
+const BASE_URL = 'https://pixabay.com/api/';
 
-  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`;
+let currentPage = 1;
+let lastQuery = '';
+
+export async function getImagesByQuery(query) {
+  if (query !== lastQuery) {
+    lastQuery = query;
+    currentPage = 1;
+  }
+
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: currentPage,
+    per_page: 15,
+  };
+
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(BASE_URL, { params });
+    currentPage += 1;
+
     return response.data;
   } catch (error) {
     console.error('Error fetching images:', error);
     throw error;
   }
 }
+
